7b3920238ee11096d9098807f0b3163824074105
utkarsh-gpt <128260025+utkarsh-gpt@users.noreply.github.com>
2024-09-03 11:44:06 -0400

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 @st.cache_resource
 def init_connection():
-    # connection_string = "mongodb+srv://evaUtkarsh:WiNE8sOH5slpkmAS@cluster0.fyr3c.mongodb.net/RxmDatabase?retryWrites=true&w=majority"
     return pymongo.MongoClient(st.secrets["mongo"]["evabot_string"])
 
 client = init_connection()
